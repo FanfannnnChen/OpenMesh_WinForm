6b6a0baa355aa4fe411c3c06cb25a644a44a1686
@fan <M11115Q26@mail.ntust.edu.tw>
2022-10-04 15:20:36 +0800

    multi color triangle

--- OpenMesh_WinFrom/Form1.h
+++ OpenMesh_WinFrom/Form1.h
@@ -2,33 +2,47 @@
 
 #include <GL/glew.h>
 #include <iostream>
+#include "Scene.h"
 using namespace std;
 
-// Shaders
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 position;\n"
-"void main()\n"
-"{\n"
-"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 color;\n"
-"void main()\n"
-"{\n"
-"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
-
-unsigned int shaderProgram;
-unsigned int VBO, VAO, EBO;
-
-float vertices[] = {
-		-0.5f, -0.5f, 0.0f, // left  
-		 0.5f, -0.5f, 0.0f, // right 
-		 0.0f,  0.5f, 0.0f  // top   
-};
-unsigned int indices[] = {  // note that we start from 0!
-	0, 1, 2  // first Triangle
-};
+//// Shaders
+//const char* vertexShaderSource = "#version 330 core\n"
+//"layout (location = 0) in vec3 aPos;\n"
+//"layout (location = 1) in vec3 aColor;\n"
+//"out vec3 ourColor;\n"
+//"void main()\n"
+//"{\n"
+//"   gl_Position = vec4(aPos, 1.0);\n"
+//"   ourColor = aColor;\n"
+//"}\0";
+//
+//const char* fragmentShaderSource = "#version 330 core\n"
+//"out vec4 FragColor;\n"
+//"in vec3 ourColor;\n"
+//"void main()\n"
+//"{\n"
+//"   FragColor = vec4(ourColor, 1.0f);\n"
+//"}\n\0";
+//
+//unsigned int shaderProgram;
+//unsigned int VBOp, VAO, EBO, VBOc;
+//
+//float vertices[] = {
+//		 0.5f, -0.5f, 0.0f,  
+//		-0.5f, -0.5f, 0.0f,  
+//		 0.0f,  0.5f, 0.0f,  
+//
+//};
+//
+//float color [] = {
+//			1.0f, 0.0f, 0.0f ,
+//			0.0f, 1.0f, 0.0f ,
+//			0.0f, 0.0f, 1.0f ,
+//};
+//
+//unsigned int indices[] = {  // note that we start from 0!
+//	0, 1, 2  // first Triangle
+//};
 
 namespace CppCLRWinformsProjekt {
 
@@ -126,94 +140,106 @@ namespace CppCLRWinformsProjekt {
 #pragma endregion
 	private: System::Void hkoglPanelControl1_Load(System::Object^ sender, System::EventArgs^ e) 
 	{
-		// ------------------------------------- Init GLEW
-		GLenum err = glewInit();
-		if (GLEW_OK != err)
-		{
-			cout << "GLEW is NOT initialized! \n ";
-		}
+		Scene_Init();
+		//// ------------------------------------- Init GLEW
+		//GLenum err = glewInit();
+		//if (GLEW_OK != err)
+		//{
+		//	cout << "GLEW is NOT initialized! \n ";
+		//}
 
-		unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-		glCompileShader(vertexShader);
+		//unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		//glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		//glCompileShader(vertexShader);
 
-		// Check for compile time errors
-		int success;
-		char infoLog[512];
-		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-		if (!success)
-		{
-			glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
+		//// Check for compile time errors
+		//int success;
+		//char infoLog[512];
+		//glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+		//if (!success)
+		//{
+		//	glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		//	std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		//}
 
-		// ------------------------------------- set Shader 
-		// Fragment shader
-		unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-		glCompileShader(fragmentShader);
+		//// ------------------------------------- set Shader 
+		//// Fragment shader
+		//unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		//glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		//glCompileShader(fragmentShader);
 
-		// Check for compile time errors
-		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-		if (!success)
-		{
-			glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
+		//// Check for compile time errors
+		//glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+		//if (!success)
+		//{
+		//	glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		//	std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+		//}
 
-		// Link shaders
-		// unsigned int shaderProgram = glCreateProgram();
-		shaderProgram = glCreateProgram();
-		glAttachShader(shaderProgram, vertexShader);
-		glAttachShader(shaderProgram, fragmentShader);
-		glLinkProgram(shaderProgram);
+		//// Link shaders
+		//// unsigned int shaderProgram = glCreateProgram();
+		//shaderProgram = glCreateProgram();
+		//glAttachShader(shaderProgram, vertexShader);
+		//glAttachShader(shaderProgram, fragmentShader);
+		//glLinkProgram(shaderProgram);
 
-		// Check for linking errors
-		glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-		if (!success) {
-			glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-		}
-		glDeleteShader(vertexShader);
-		glDeleteShader(fragmentShader);
+		//// Check for linking errors
+		//glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+		//if (!success) {
+		//	glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		//	std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+		//}
+		//glDeleteShader(vertexShader);
+		//glDeleteShader(fragmentShader);
 
-		// -------------------------------------set VAO VBO 
-		glGenVertexArrays(1, &VAO);
-		glGenBuffers(1, &VBO);
+		//// -------------------------------------set VAO VBO 
+		//glGenVertexArrays(1, &VAO);
+		//glGenBuffers(1, &VBOp);
+		//glGenBuffers(1, &VBOc);
 
-		glGenBuffers(1, &EBO);
-		// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-		glBindVertexArray(VAO);
+		//// glGenBuffers(1, &EBO);
+		//// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+		//glBindVertexArray(VAO);
 
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		//glBindBuffer(GL_ARRAY_BUFFER, VBOp);
+		//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(0);
+		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		//glEnableVertexAttribArray(0);
 
-		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		//glBindBuffer(GL_ARRAY_BUFFER, VBOc);
+		//glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
 
-		// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-		// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-		glBindVertexArray(0);
+		//// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//// glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		//glEnableVertexAttribArray(1);
+
+		//
+		//// UNBIND
+		//// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+		//// glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		//// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+		//// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+		//// glBindVertexArray(0);
 
 
 	}
 	private: System::Void hkoglPanelControl1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e)
 	{
-		glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		render();
+		//glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+		//glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-		// Disable vertex attribute arrays
-		// glDisableVertexAttribArray(0);
-		// glDisableVertexAttribArray(1);
+		//glUseProgram(shaderProgram);
+		//glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		//// glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		//// Disable vertex attribute arrays
+		//// glDisableVertexAttribArray(0);
+		//// glDisableVertexAttribArray(1);
 	}
 	
 	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e)
